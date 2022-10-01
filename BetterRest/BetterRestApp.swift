7e27de5fe39ce9b00ea6742f812f7787986bc5ac
William Young <wadyoung@outlook.com>
2022-10-01 07:17:15 +0100

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by EngineerBetter on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
